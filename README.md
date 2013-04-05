c169e33ecbba95cbab0755b29bf2456cb8a2a85b
Jim Chen <nchen@mozilla.com>
2013-04-05 12:55:54 -0400

    initial anr.py

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-anr-analyzer
-============
+ANR Report Analyzer
+===================
 
-Scripts to parse and group ANR reports
+Scripts to parse and analyze ANR reports
+
+## anr.py
+
+Parses ANR reports from the input file and groups them into similar reports, based on a threshold value.
+
+Each line of the input file is an ANR report in JSON format (see ANRReporter.java).
+
+The threshold value from 0.0 to 1.0 indicates how similar reports have to be in order to be grouped together; 0.0 indicates two reports are totally distinct, and 1.0 indicates two reports are identical. In practice, unrelated reports have similarity of around 0.3 and below, and related reports have similarity of around 0.7 and above; a good starting threshold is around 0.6.
+
+#### Usage
+
+    python anr.py <threshold> <input>
+
+      <threshold>     grouping threshold from 0.0 to 1.0
+      <input>         input file of ANR reports
+
---
+++ anr.py
@@ -0,0 +1,381 @@
+#!/usr/bin/python2
+
+import array, json, re
+
+DEBUG = False
+
+ANR_JAVA_METHOD = re.compile(
+    r'([0-9a-zA-Z_\.\$]+)(?:.*?\((.*?)(?::(.*))?\))?')
+ANR_NATIVE_FUNCTION = re.compile(
+    r'(\d+).*? ([0-9a-fA-F]+)(?:.*? (\S*[/\.]\S+))?(?:.*? \((.+)\))?')
+
+class ANRReport:
+
+    class Thread:
+
+        def __init__(self, name, states, props, stack):
+            self.name = name
+            self.states = list(states)
+            self.props = dict(props)
+            self.stack = list(stack)
+
+        def __getitem__(self, index):
+            if index in self.props:
+                return self.props[index]
+            return index in self.states
+
+        def _neMatch(self, selfStack, otherStack):
+            # performs a "not-equals" matching using dynamic time warping
+            if len(selfStack) == 1:
+                return sum(selfStack[0] != s for s in otherStack)
+            if len(otherStack) == 1:
+                return sum(s != otherStack[0] for s in selfStack)
+
+            left = array.array('d', (0.0 for s in selfStack))
+            right = array.array('d', left)
+            if DEBUG:
+                print ' DTW of size %d and %d' % (
+                    len(selfStack), len(otherStack))
+
+            # first element of first column
+            right[0] = (selfStack[0] != otherStack[0])
+            # other elements of first column
+            for j in range(1, len(selfStack)):
+                right[j] = (selfStack[j] != otherStack[0]) + right[j - 1]
+            if DEBUG:
+                print '  column 0 min at %s' % (str(min(zip(
+                    range(len(right)), right), key=lambda k: right[k[0]])))
+            # other columnns
+            for i in range(1, len(otherStack)):
+                # move to next column by swapping
+                left, right = right, left
+                # first element of other column
+                right[0] = (selfStack[0] != otherStack[i]) + left[0]
+                # other elements of other column
+                for j in range(1, len(selfStack)):
+                    right[j] = (selfStack[j] != otherStack[i]) + min(
+                            left[j], left[j - 1], right[j - 1])
+                if DEBUG:
+                    print '  column %d min at %s' % (i, str(min(zip(
+                        range(len(right)), right), key=lambda k: right[k[0]])))
+            # result is the sum at the other corner
+            return right[-1]
+
+        def __eq__(self, other):
+            if not other or not self.stack or not other.stack:
+                return 0.0
+            if DEBUG:
+                print 'Comparing thread %s to thread %s' % (
+                    self.name, other.name)
+            # ensure commutativity
+            if len(self.stack) < len(other.stack):
+                return other == self
+            if len(self.stack) == len(other.stack) and self.name < other.name:
+                return other == self
+            aStart = bStart = 0
+            if not self.stack[0].isNative or not other.stack[0].isNative:
+                # don't compare native stack if either doesn't have it
+                aStart = next(i for i in range(len(self.stack))
+                    if not self.stack[i].isNative)
+                bStart = next(i for i in range(len(other.stack))
+                    if not other.stack[i].isNative)
+            # find an initial point of comparison
+            aIndex = min(range(aStart, len(self.stack)), key=lambda i:
+                    i + (len(self.stack) if self.stack[i].isNative else 0)) - 1
+            bIndex = None
+            while not bIndex or (aIndex < len(self.stack) - 1 and
+                    (self.stack[aIndex] == other.stack[bIndex]) < 0.7):
+                aIndex += 1
+                bIndex = max(range(bStart, len(other.stack)),
+                        key=lambda i: other.stack[i] == self.stack[aIndex])
+            if DEBUG:
+                print ' starting at %d and %d with %f' % (
+                    aIndex, bIndex, self.stack[aIndex] == other.stack[bIndex])
+            return (1.0 - (
+                    self._neMatch(self.stack[aStart: aIndex + 1],
+                        other.stack[bStart: bIndex + 1]) +
+                    self._neMatch(self.stack[aIndex:], other.stack[bIndex:])) /
+                    (max(len(self.stack) - aStart,
+                        len(other.stack) - bStart) + 1))
+
+        def __ne__(self, other):
+            return 1.0 - (self == other)
+
+    class StackFrame:
+
+        def __init__(self, frame, isNative):
+            self.isNative = isNative
+            self.javaMethod = self.javaFile = self.javaLine = None
+            self.nativeId = self.nativeAddress = None
+            self.nativeLib = self.nativeFunction = None
+            try:
+                if isNative:
+                    self._initNative(frame)
+                else:
+                    self._initJava(frame)
+            except IndexError:
+                return
+
+        def _initJava(self, frame):
+            # android.os.Handler.handleCallback(Handler.java:615)
+            tokens = ANR_JAVA_METHOD.split(frame.strip())
+            self.javaMethod = tokens[1]
+            self.javaFile = tokens[2]
+            self.javaLine = tokens[3]
+
+        def _initNative(self, frame):
+            # 03 pc 00023f7d /system/lib/libgui.so \
+            #  (android::SensorEventQueue::waitForEvent() const+36)
+            tokens = ANR_NATIVE_FUNCTION.split(frame.strip())
+            self.nativeId = int(tokens[1], 10)
+            self.nativeAddress = int(tokens[2], 16)
+            self.nativeLib = tokens[3]
+            self.nativeFunction = tokens[4]
+
+        def __str__(self):
+            if self.isNative:
+                return self.nativeFunction if self.nativeFunction else '(native)'
+            return self.javaMethod if self.javaMethod else '(java)'
+
+        def _eqNative(self, other):
+            aLib = self.nativeLib
+            bLib = other.nativeLib
+            if not aLib or not bLib:
+                # give function name a chance
+                return 0.1 * self._eqNativeFunction(
+                    self.nativeFunction, other.nativeFunction)
+            if (aLib.find('/') < 0) != (bLib.find('/') < 0):
+                # strip directory
+                aLib = aLib[aLib.rfind('/') + 1:]
+                bLib = bLib[bLib.rfind('/') + 1:]
+            if aLib and bLib and aLib == bLib:
+                return self._eqNativeFunction(
+                    self.nativeFunction, other.nativeFunction)
+            # compare without directory
+            aLib = aLib[aLib.rfind('/') + 1:]
+            bLib = bLib[bLib.rfind('/') + 1:]
+            if aLib and bLib and aLib == bLib:
+                return 0.9 * self._eqNativeFunction(
+                    self.nativeFunction, other.nativeFunction)
+            return 0.0
+
+        def _eqNativeFunction(self, a, b):
+            if not a or not b:
+                return 1.0 if not a and not b else 0.0
+            # a and b are in the form ns::func(arg)+line
+            if (a.find('+') < 0) != (b.find('+') < 0):
+                # strip line numbers
+                a = a.partition('+')[0]
+                b = b.partition('+')[0]
+            if (a.find('(') < 0) != (b.find('(') < 0):
+                # strip arguments
+                a = a.partition('(')[0]
+                b = b.partition('(')[0]
+            if (a.find(':') < 0) != (b.find(':') < 0):
+                # strip namespaces
+                a = a[a.rfind(':') + 1:]
+                b = b[b.rfind(':') + 1:]
+            if a and b and a == b:
+                return 1.0
+            # compare without line number
+            a = a.partition('+')[0]
+            b = b.partition('+')[0]
+            if a and b and a == b:
+                return 0.9
+            # compare without arguments
+            a = a.partition('(')[0]
+            b = b.partition('(')[0]
+            if a and b and a == b:
+                return 0.7
+            # compare without namespaces
+            a = a[a.rfind(':') + 1:]
+            b = b[b.rfind(':') + 1:]
+            if a and b and a == b:
+                return 0.4
+            return 0.0
+
+        def _eqJava(self, other):
+            aLine = self.javaLine
+            bLine = other.javaLine
+            if aLine and bLine:
+                aLine = int(self.javaLine.strip('~'), 10)
+                bLine = int(other.javaLine.strip('~'), 10)
+                if 10 * abs(aLine - bLine) / max(aLine, bLine) > 0:
+                    return 0.8 * self._eqJavaMethod(
+                            self.javaMethod, other.javaMethod)
+            return self._eqJavaMethod(self.javaMethod, other.javaMethod)
+
+        def _eqJavaMethod(self, a, b):
+            # we can still compare files
+            if not a or not b:
+                return 0.1
+            # a and b are in the form pkg.cls$child.method
+            if a and b and a == b:
+                return 1.0
+            aTokens = a.split('.')
+            aClass = aTokens[-2].partition('$')
+            aMethod = aTokens[-1]
+            bTokens = b.split('.') # package, 0.2 of weight
+            bClass = bTokens[-2].partition('$') # 0.3+0.1 of weight
+            bMethod = bTokens[-1] # 0.4 of weight
+            ret = 0.4 if aMethod == bMethod else 0.0
+            ret += ((0.3 + (0.1 if aClass[-1] == bClass[-1] else 0.0))
+                    if aClass[0] == bClass[0] else 0.0)
+            # compare package
+            ret += min(0.2, 0.2 * next(i
+                    for i in range(min(len(aTokens), len(bTokens)))
+                    if aTokens[i] != bTokens[i]) /
+                    (max(len(aTokens), len(bTokens)) - 2))
+            return ret
+
+        def __eq__(self, other):
+            if DEBUG:
+                print '   comparing %s to %s' % (self, other)
+            if self.isNative != other.isNative:
+                return 0.0
+            if self.isNative:
+                return self._eqNative(other)
+            return self._eqJava(other)
+
+        def __ne__(self, other):
+            return 1.0 - (self == other)
+
+    def _parseLine(self, line, name, states, props, stack):
+        line = line.strip()
+        if not line:
+            return
+
+        if line.startswith('"'):
+            # new thread
+            if name:
+                # save previous thread
+                self.threads.append(ANRReport.Thread(name, states, props, stack))
+                del states[:]
+                props.clear()
+                del stack[:]
+            # "main" prio=5 tid=1 SUSPENDED
+            tokens = line[1:].partition('"')
+            name = tokens[0]
+            for token in tokens[2].split():
+                if '=' not in token:
+                    states.append(token)
+                    continue
+                prop = token.partition('=')
+                props[prop[0]] = prop[2]
+            return name
+
+        if name and (line.startswith('|') or line.startswith('>')):
+            # | group="main" sCount=1 dsCount=0 obj=0x41734508 self=0x41723fb0
+            # | sysTid=16806 nice=0 sched=0/0 cgrp=apps handle=1075207984
+            # | schedstat=( 22141839338 608612447 4633 ) utm=2191 stm=22 core=3
+            line = line[1:]
+            if '=' in line:
+                # properties
+                line = ''.join([(s if i & 1 else s.replace(' ', '\0'))
+                    for i, s in enumerate(line.split('"'))])
+                for prop in line.split('\0'):
+                    if not prop:
+                        continue
+                    prop = prop.partition('=')
+                    if not prop[1]:
+                        states.append(prop[0])
+                        continue
+                    props[prop[0]] = prop[2]
+            return
+
+        if name and line.startswith('at '):
+            stack.append(ANRReport.StackFrame(line[3:], isNative=False))
+            return
+
+        if name and line.startswith('#'):
+            stack.append(ANRReport.StackFrame(line[1:], isNative=True))
+            return
+
+    def __init__(self, raw):
+        self.rawData = json.loads(raw)
+        self.threads = []
+        name = None
+        states = []
+        props = {}
+        stack = []
+        # parse ANR
+        for line in self.rawData['androidANR'].splitlines():
+            tmp = self._parseLine(line, name, states, props, stack)
+            name = tmp if tmp else name
+        if name:
+            # save last thread
+            self.threads.append(ANRReport.Thread(name, states, props, stack))
+
+    @property
+    def log(self):
+        return self['androidLogcat']
+
+    def getThread(self, name):
+        try:
+            return next(t for t in self.threads if t.name == name)
+        except StopIteration:
+            return None
+
+    @property
+    def mainThread(self):
+        return self.getThread('main')
+
+    @property
+    def detail(self):
+        t = self.mainThread
+        return 0 if not t else len(t.stack)
+
+    def __eq__(self, other):
+        return self.mainThread == other.mainThread
+
+def cluster(anrs, threshold):
+    if len(anrs) == 1:
+        return [anrs]
+    left = cluster(anrs[:len(anrs) / 2], threshold)
+    right = cluster(anrs[len(anrs) / 2:], threshold)
+    ret = []
+    for r in right:
+        # extend left cluster if right cluster matches left cluster
+        i = next((i for i in range(len(left))
+            if (left[i][0] == r[0]) >= threshold), -1)
+        if i == -1:
+            ret.append(r)
+            continue
+        l = left.pop(i)
+        if l[0].detail > r[0].detail:
+            l.extend(r)
+            ret.append(l)
+        else:
+            r.extend(l)
+            ret.append(r)
+    ret.extend(left)
+    return ret
+
+if __name__ == '__main__':
+
+    import sys, time
+
+    if len(sys.argv) != 3:
+        print 'Usage: %s <threshold> <input>' % (sys.argv[0])
+        sys.exit(1)
+
+    starttime = time.time()
+
+    threshold = float(sys.argv[1])
+    with open(sys.argv[2], 'r') as f:
+        anrs = [ANRReport(l) for l in f]
+
+    clusters = cluster(anrs, threshold)
+    clusters.sort(key=lambda c: len(c))
+
+    for i, clus in enumerate(clusters):
+        print 'Cluster %d: %d member(s), sample stack:' % (i + 1, len(clus))
+        mainThread = clus[0].mainThread
+        if mainThread:
+            for s in mainThread.stack:
+                print ' ' + str(s)
+        print
+
+    print 'Took %f seconds\n' % (time.time() - starttime)
+    sys.exit(1)
+
