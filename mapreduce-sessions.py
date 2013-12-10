eb05aeb23bc6bc81f58fb06a604da64c78d52133
Jim Chen <nchen@mozilla.com>
2013-12-10 01:16:32 -0500

    use uptime instead of sessions

--- mapreduce-sessions.py
+++ mapreduce-sessions.py
@@ -2,17 +2,21 @@ import json
 
 def map(slug, dims, value, context):
     ping = json.loads(value)
-    if 'info' not in ping:
+    if ('info' not in ping or
+        'simpleMeasurements' not in ping or
+        'uptime' not in ping['simpleMeasurements']):
         return
+    info = ping['info']
+    uptime = ping['simpleMeasurements']['uptime']
     for i, dim in enumerate(dims):
-        context.write((i, dim), ping['info'])
+        context.write((i, dim), (uptime, ping['info']))
 
 def reduce(key, values, context):
     if not values:
         return
     aggregate = {}
-    for value in values:
+    for uptime, value in values:
         for k, v in value.iteritems():
             bucket = aggregate.setdefault(k, {})
-            bucket[v] = bucket.get(v, 0) + 1
+            bucket[v] = bucket.get(v, 0) + uptime
     context.write(json.dumps(list(key)), json.dumps(aggregate))
